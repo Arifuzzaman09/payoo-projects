ee5851486d04a88cb3bd5403882c329d25a4217a
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-02-19 15:07:03 +0600

    login js added

---
+++ payoo.js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    let numberInput = document.getElementById('number-input');
+    let number = numberInput.value;
+    console.log(number)
+
+    let digitInput = document.getElementById('digit-input');
+    let pin = digitInput.value;
+    console.log(pin)
+
+    if (number === '01300666763' && pin === '1234') {
+        alert('login seccesfull')
+        window.location.assign('/home.html')
+    } else {
+        alert('login failed')
+        return;
+    }
+})
